2fad85056c80b6a1cc1b619905762e2b4116f71d
gavin@ezynets.com <jsguxin@gmail.com>
2026-07-03 18:55:21 +0800

    Delete ipqmd directory

--- ipqmd/config.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-// 高德地图Web服务密钥（天气接口）
-$gaode_key = "";
-// 腾讯位置WebService密钥（IP定位接口）
-$tencent_map_key = "";
-// 腾讯后台域名白名单完整域名（带https）
-$site_referer = "https://www.example.com";
-// 图片顶部欢迎标语
-$site_name = "欢迎来到example.com";
